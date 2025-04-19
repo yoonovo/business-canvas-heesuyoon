e18be1c39a7ee525978255e1417ed3129ce0f61b
yoonovo <yhsgmltn116@gmail.com>
2025-04-19 18:09:03 +0900

    refactor: 안쓰는 코드 정리

--- src/pages/RecordList.tsx
+++ src/pages/RecordList.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import dayjs from "dayjs";
 import { v4 as uuidv4 } from "uuid";
-import { Button, Flex, Typography, Modal, ModalProps } from "antd";
-import { DeleteTwoTone, PlusOutlined } from "@ant-design/icons";
+import { Modal, ModalProps } from "antd";
+import { DeleteTwoTone } from "@ant-design/icons";
 
 import { CommonTable, FormModal } from "@/components";
 import { recordColumns } from "@/constants/recordColumns";
